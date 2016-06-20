aee0cab0baff765f6d1005889197f36f8f6d6fc8
cam-shotter <cameronshotter@gmail.com>
2016-06-20 16:15:48 +1200

    made counter work

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -29,6 +30,10 @@ function makeBlue (evt) {
 }
 
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
@@ -38,7 +43,17 @@ function updateCounts () {
   }
 
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var dots = document.getElementsByClassName('board')[0].children
 
+  for (var i = 0; i < dots.length; i++) {
+    if (dots[i].classList.contains('blue')) {
+      totals.blue++
+    } else if (dots[i].classList.contains('green')) {
+      totals.green++
+    } else if (dots[i].classList.contains('invisible')) {
+      totals.invisible++
+    }
+  }
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
